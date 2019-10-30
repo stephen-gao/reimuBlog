79f5d8c10dcd4f69c568f588f3aa6c10591daca9
gaosheng <gaosheng@253.com>
2019-10-30 20:52:44 +0800

    权限 用户 js请求封装

--- src/main/java/com/reimu/utils/MailUtil.java
+++ src/main/java/com/reimu/utils/MailUtil.java
@@ -21,8 +21,8 @@ public class MailUtil {
      * PS: 某些邮箱服务器为了增加邮箱本身密码的安全性，给 SMTP 客户端设置了独立密码（有的邮箱称为“授权码”）,
      * 对于开启了独立密码的邮箱, 这里的邮箱密码必需使用这个独立密码（授权码）。
      */
-    private static String EmailAccount = "gaoshengok@126.com";
-    private static String EmailPassword = "126shouquanma";
+    private static String EmailAccount = "xxxx";
+    private static String EmailPassword = "xxxx";
 
     /**
      * 发件人邮箱的 SMTP 服务器地址, 必须准确, 不同邮件服务器地址不同, 一般(只是一般, 绝非绝对)格式为: smtp.xxx.com
@@ -33,7 +33,7 @@ public class MailUtil {
     /**
      * 收件人邮箱（替换为自己知道的有效邮箱）
      */
-    private static String receiveMailAccount = "gaoshengok@sina.com";
+    private static String receiveMailAccount = "xxxxx";
 
     public static void main(String[] args) throws Exception {
         String msg = "你部署在187服务器上的程序意外中断，请尽快处理";
